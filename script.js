cb214bc893e645627bdda54420f887129d72ea42
Pranav Tharoor <pranav.tharoor@gmail.com>
2017-08-30 16:42:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+	e.preventDefault();
+	const text = (this.querySelector('[name=item]')).value;
+	const item = {
+		text,
+		done: false
+	};
+	items.push(item);
+	populateList(items, itemsList);
+	localStorage.setItem('items', JSON.stringify(items));
+	this.reset();
+}
+
+function populateList(plates = [], platesList) {
+	platesList.innerHTML = plates.map((plate, i) => {
+		check = plate.done ? 'checked' : '';
+		return `
+				<tr>
+				<td><input type="checkbox" data-index=` + i + ` id="item` + i + `" ` + check + ` /></td>
+				<td class="toDoText"><label for="item` + i + `">` + plate.text + `</label></td>
+				<td class="removeButtonContainer"><button onclick="removeItem(` + i +`)">Remove</button></td>
+				</tr>
+		`;
+	}).join('');
+}
+
+function toggleDone(e) {
+	if (!e.target.matches('input')) return;
+	const el = e.target;
+	const index = el.dataset.index;
+	items[index].done = !items[index].done;
+	localStorage.setItem('items', JSON.stringify(items));
+	populateList(items, itemsList);
+}
+
+function removeItem(i) {
+	items.splice(i, 1);
+	populateList(items, itemsList);
+	localStorage.setItem('items', JSON.stringify(items));
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+populateList(items, itemsList);
